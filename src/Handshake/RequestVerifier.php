eda1004f4057c7326de464341ac13ecf059fee45
Marcel Pociot <m.pociot@gmail.com>
2026-01-22 10:40:37 +0100

    Update RequestVerifier.php

--- src/Handshake/RequestVerifier.php
+++ src/Handshake/RequestVerifier.php
@@ -45,7 +45,7 @@ class RequestVerifier {
      * @return bool
      */
     public function verifyHTTPVersion($val) {
-        return (1.1 <= (double)$val);
+        return (1.1 <= (float)$val);
     }
 
     /**
